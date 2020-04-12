c094ecf4827268968e83495be77895fd9f8fbdab
abangkis <abangkis@gmail.com>
2020-04-12 14:47:52 +0700

    Add online-offline status. Please note this will check all adapter. So if you have virtual adapter like docker. It will still be online although your hardware adapter is offline

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -8,6 +8,7 @@ import kotlinx.html.js.tr
 import kotlinx.html.td
 import kotlinx.html.th
 import org.w3c.dom.Element
+import org.w3c.dom.events.EventListener
 import kotlin.browser.document
 import kotlin.browser.window
 
@@ -15,8 +16,14 @@ fun main() {
 //    document.write("Hello, world!")
 //    document.bgColor = "FFAA12"
 
+// Update the online status icon based on connectivity
+    window.addEventListener("online", EventListener { updateIndicator("from online") })
+    window.addEventListener("offline", EventListener { updateIndicator("from offline") })
+
     window.onload = {
-        var table = document.getElementById("tableBody")
+        updateIndicator("from onload")
+
+        val tableIpBody = document.getElementById("ipBody")
 
         MainScope().launch {
             val ipInfo = fetchIpInfo()
@@ -24,16 +31,35 @@ fun main() {
                 val info = it.split("=")
                 if(info.size == 2){
                     when(info[0]) {
-                        "ip", "ts", "loc", "uag" -> addTr(info, table)
+                        "ip", "ts", "loc", "uag" -> addTr(info, tableIpBody)
+                    }
+                }
+            }
+        }
+
+//        var connection = window.navigator.connection || window.navigator.mozConnection || window.navigator.webkitConnection
+//        if(connection) {
+//            connection.addEventListener('change', logNetworkInfo);
+//            logNetworkInfo()
+//        }
+
+        val tableNetworkBody = document.getElementById("networkBody")
+
+
+
+
+        MainScope().launch {
+            val ipInfo = fetchIpInfo()
+            ipInfo.split("\n").forEach {
+                val info = it.split("=")
+                if(info.size == 2){
+                    when(info[0]) {
+                        "ip", "ts", "loc", "uag" -> addTr(info, tableNetworkBody)
                     }
                 }
             }
         }
     }
-
-//
-//    val email = document.getElementById("email") as HTMLInputElement
-//    email.value = "hadi@jetbrains.com"
 }
 
 private fun addTr(info: List<String>, table: Element?) {
@@ -55,8 +81,44 @@ suspend fun fetchIpInfo() = window.fetch("https://www.cloudflare.com/cdn-cgi/tra
         .text()
         .await()
         .toString()
-//        .json()
-//        .await()
-//        .unsafeCast<Array<JphUser>>()
+
+/**
+ *  Please note this will check all adapter. So if you have virtual adapter like docker.
+ *  It will still be online although your hardware adapter is offline
+ */
+fun  updateIndicator(source: String) {
+    console.log("from $source")
+    val status = document.getElementById("status")
+    if(window.navigator.onLine) {
+        status?.textContent = "Online"
+//        status?.style = "color:green"
+    }
+    else {
+        status?.textContent = "Offline"
+//        status?.style = "color:red"
+    }
+}
+
+//fun logNetworkInfo() {
+//    // Network type that browser uses
+//    log('         type: ' + navigator.connection.type);
+//
+//    // Effective bandwidth estimate
+//    log('     downlink: ' + navigator.connection.downlink + 'Mb/s');
+//
+//    // Effective round-trip time estimate
+//    log('          rtt: ' + navigator.connection.rtt + 'ms');
+//
+//    // Upper bound on the downlink speed of the first network hop
+//    log('  downlinkMax: ' + navigator.connection.downlinkMax + 'Mb/s');
+//
+//    // Effective connection type determined using a combination of recently
+//    // observed rtt and downlink values: ' +
+//    log('effectiveType: ' + navigator.connection.effectiveType);
+//
+//    // True if the user has requested a reduced data usage mode from the user
+//    // agent.
+//    log('     saveData: ' + navigator.connection.saveData);
+//}
 
 
