51f5742de1fbdff206d95cb3f4b8e6bef2c56084
abangkis <abangkis@gmail.com>
2020-04-18 11:13:41 +0700

    Refactor main code to some function. Parse timestamp to human readable format. Add saveData network information

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -14,6 +14,7 @@ import org.w3c.dom.Element
 import org.w3c.dom.events.EventListener
 import kotlin.browser.document
 import kotlin.browser.window
+import kotlin.js.Date
 
 @ImplicitReflectionSerializer
 fun main() {
@@ -21,41 +22,79 @@ fun main() {
 //    document.bgColor = "FFAA12"
 
 // Update the online status icon based on connectivity
-    window.addEventListener("online", EventListener { updateIndicator("from online") })
-    window.addEventListener("offline", EventListener { updateIndicator("from offline") })
+    window.addEventListener("online", EventListener { updateStatus("from online") })
+    window.addEventListener("offline", EventListener { updateStatus("from offline") })
 
     window.onload = {
-        updateIndicator("from onload")
+        updateStatus("from onload")
+        updateNetworkBody()
+    }
+}
 
-        val tableIpBody = document.getElementById("ipBody")
+fun updateStatus(source: String) {
+    updateConnectionStatus(source)
+    updateIpBody()
+}
 
-        MainScope().launch {
-            val ipInfo = fetchIpInfo()
-            ipInfo.split("\n").forEach {
-                val info = it.split("=")
-                if (info.size == 2) {
-                    when (info[0]) {
-                        "ip", "ts", "loc", "uag" -> addTr(Pair(info[0], info[1]), tableIpBody)
+/**
+ *  Please note this will check all adapter. So if you have virtual adapter like docker.
+ *  It will still be online although your hardware adapter is offline
+ */
+fun updateConnectionStatus(source: String) {
+    console.log("from $source")
+    val status = document.getElementById("connectionStatus")
+    if (window.navigator.onLine) {
+        status?.textContent = "Connection Status: Online"
+//        status?.style = "color:green"
+    } else {
+        status?.textContent = "Connection Status: Offline"
+//        status?.style = "color:red"
+    }
+}
+
+private fun updateIpBody() {
+    val tableIpBody = document.getElementById("ipBody")
+
+    MainScope().launch {
+        val ipInfo = fetchIpInfo()
+
+        val status = document.getElementById("internetStatus")
+        status?.textContent = "Internet Status: Online"
+
+        ipInfo.split("\n").forEach {
+            val info = it.split("=")
+            if (info.size == 2) {
+                when (info[0]) {
+                    "ip", "loc", "uag" -> addTr(Pair(info[0], info[1]), tableIpBody)
+                    "ts" -> {
+                        console.log("ts ${info[1]}")
+                        val ts = info[1].toDouble() * 1000 // change from second to millisecond
+                        console.log("ts $ts")
+                        addTr(Pair("ts", Date(ts).toString()), tableIpBody)
                     }
                 }
             }
         }
+    }
+}
 
-        val connection = getNavigatorConnection()
-        if(connection != null) {
-            val networkInfo  = parseConnection(connection)
+@ImplicitReflectionSerializer
+private fun updateNetworkBody() {
+    val connection = getNavigatorConnection()
+    if (connection != null) {
+        val networkInfo = parseConnection(connection)
 
-            val tableNetworkBody = document.getElementById("networkBody")
-            addTr(Pair("Effective Type", networkInfo.effectiveType), tableNetworkBody)
-            addTr(Pair("Downlink", "${networkInfo.downlink}Mb/s"), tableNetworkBody)
-            addTr(Pair("Rtt", "${networkInfo.rtt}ms"), tableNetworkBody)
+        val tableNetworkBody = document.getElementById("networkBody")
+        addTr(Pair("Effective Type", networkInfo.effectiveType), tableNetworkBody)
+        addTr(Pair("Down link", "${networkInfo.downlink}Mb/s"), tableNetworkBody)
+        addTr(Pair("Round trip time", "${networkInfo.rtt}ms"), tableNetworkBody)
+        addTr(Pair("Save Data", "${networkInfo.saveData}"), tableNetworkBody)
 
 //            connection.addEventListener('change' function() {
 //                // network change
 //            });
 //            connection.addEventListener('change', logNetworkInfo);
 //            logNetworkInfo()
-        }
     }
 }
 
@@ -67,7 +106,8 @@ fun parseConnection(connection: Unit): NetworkInformation {
 @Serializable
 class NetworkInformation(val effectiveType: String,
                          val downlink: Int,
-                         val rtt: Int
+                         val rtt: Int,
+                         val saveData: Boolean
 )
 
 private fun addTr(info: Pair<String, String>, table: Element?) {
@@ -90,21 +130,7 @@ suspend fun fetchIpInfo() = window.fetch("https://www.cloudflare.com/cdn-cgi/tra
         .await()
         .toString()
 
-/**
- *  Please note this will check all adapter. So if you have virtual adapter like docker.
- *  It will still be online although your hardware adapter is offline
- */
-fun updateIndicator(source: String) {
-    console.log("from $source")
-    val status = document.getElementById("status")
-    if (window.navigator.onLine) {
-        status?.textContent = "Online"
-//        status?.style = "color:green"
-    } else {
-        status?.textContent = "Offline"
-//        status?.style = "color:red"
-    }
-}
+
 
 fun getNavigatorConnection() : Unit? {
     // fixme kotlin/js doesn't have navigator.connection object yet. So we must create it manually
